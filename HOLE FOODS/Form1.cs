7b78f242f51d53f22ca4ed94747aa7f0a3dc2dc1
mPerier <=>
2020-10-25 16:55:07 +0100

    Ajout du code de validerPB_Click

--- HOLE FOODS/Form1.cs
+++ HOLE FOODS/Form1.cs
@@ -31,6 +31,7 @@ namespace HOLE_FOODS
         private void NVPanier_PB_Click(object sender, EventArgs e)
         {
             nosProduits = new Panier();
+            ticketActuel = new Ticket();
 
         }
         private void Produit_LB_SelectedIndexChanged(object sender, EventArgs e)
@@ -50,7 +51,13 @@ namespace HOLE_FOODS
 
         private void validerLegumeButton_Click(object sender, EventArgs e)
         {
-
+            // On initialise un nouveau produit à partir des éléments de l'interface
+            Produit produitAjout;
+            produitAjout = new Produit(Produit_LB.SelectedItem.ToString(), Convert.ToDouble(Prix_TB.Text), Convert.ToDouble(Poids_TB.Text));
+            // Puis on l'ajoute au panier
+            nosProduits.ajouterPanier(produitAjout);
+            // Avant d'ajouter sa decription au ticket
+            ticketActuel.ajouterLigne(produitAjout.extraireString());
         }
 
         private void genererTicketButton_Click(object sender, EventArgs e)
