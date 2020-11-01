a82c61083151db8b610ac2c1b5bcab3d9ac45f69
Nathan TURPIN <n.turpin@net.estia.fr>
2020-11-01 16:44:04 +0100

    ajout des getTicketPath

--- HOLE FOODS/Form1.Designer.cs
+++ HOLE FOODS/Form1.Designer.cs
@@ -42,11 +42,13 @@
             this.label4 = new System.Windows.Forms.Label();
             this.ticketPath_PB = new System.Windows.Forms.Button();
             this.ticketPathRecover_FD = new System.Windows.Forms.FolderBrowserDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Prix_TB
             // 
-            this.Prix_TB.Location = new System.Drawing.Point(227, 113);
+            this.Prix_TB.Location = new System.Drawing.Point(323, 108);
             this.Prix_TB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Prix_TB.Name = "Prix_TB";
             this.Prix_TB.Size = new System.Drawing.Size(95, 22);
@@ -55,7 +57,7 @@
             // 
             // Poids_TB
             // 
-            this.Poids_TB.Location = new System.Drawing.Point(368, 112);
+            this.Poids_TB.Location = new System.Drawing.Point(464, 107);
             this.Poids_TB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Poids_TB.Name = "Poids_TB";
             this.Poids_TB.Size = new System.Drawing.Size(95, 22);
@@ -64,7 +66,7 @@
             // 
             // validerLegumeButton
             // 
-            this.validerLegumeButton.Location = new System.Drawing.Point(485, 112);
+            this.validerLegumeButton.Location = new System.Drawing.Point(600, 106);
             this.validerLegumeButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.validerLegumeButton.Name = "validerLegumeButton";
             this.validerLegumeButton.Size = new System.Drawing.Size(75, 23);
@@ -77,7 +79,7 @@
             // 
             this.Produit_LB.FormattingEnabled = true;
             this.Produit_LB.ItemHeight = 16;
-            this.Produit_LB.Location = new System.Drawing.Point(83, 112);
+            this.Produit_LB.Location = new System.Drawing.Point(179, 107);
             this.Produit_LB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Produit_LB.Name = "Produit_LB";
             this.Produit_LB.Size = new System.Drawing.Size(76, 52);
@@ -85,7 +87,7 @@
             // 
             // genererTicketButton
             // 
-            this.genererTicketButton.Location = new System.Drawing.Point(317, 268);
+            this.genererTicketButton.Location = new System.Drawing.Point(464, 214);
             this.genererTicketButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.genererTicketButton.Name = "genererTicketButton";
             this.genererTicketButton.Size = new System.Drawing.Size(105, 23);
@@ -96,7 +98,7 @@
             // 
             // TotalPanierTextBox
             // 
-            this.TotalPanierTextBox.Location = new System.Drawing.Point(216, 229);
+            this.TotalPanierTextBox.Location = new System.Drawing.Point(363, 175);
             this.TotalPanierTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.TotalPanierTextBox.Name = "TotalPanierTextBox";
             this.TotalPanierTextBox.ReadOnly = true;
@@ -106,7 +108,7 @@
             // 
             // Total_TB
             // 
-            this.Total_TB.Location = new System.Drawing.Point(317, 229);
+            this.Total_TB.Location = new System.Drawing.Point(464, 175);
             this.Total_TB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Total_TB.Name = "Total_TB";
             this.Total_TB.Size = new System.Drawing.Size(95, 22);
@@ -114,7 +116,7 @@
             // 
             // NVPanier_PB
             // 
-            this.NVPanier_PB.Location = new System.Drawing.Point(55, 21);
+            this.NVPanier_PB.Location = new System.Drawing.Point(12, 107);
             this.NVPanier_PB.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NVPanier_PB.Name = "NVPanier_PB";
             this.NVPanier_PB.Size = new System.Drawing.Size(139, 49);
@@ -126,7 +128,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(255, 92);
+            this.label1.Location = new System.Drawing.Point(351, 87);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 17);
             this.label1.TabIndex = 15;
@@ -135,7 +137,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(391, 92);
+            this.label2.Location = new System.Drawing.Point(487, 87);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 17);
             this.label2.TabIndex = 16;
@@ -144,7 +146,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(325, 114);
+            this.label3.Location = new System.Drawing.Point(421, 109);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(16, 17);
             this.label3.TabIndex = 17;
@@ -153,7 +155,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(419, 231);
+            this.label4.Location = new System.Drawing.Point(566, 177);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(16, 17);
             this.label4.TabIndex = 18;
@@ -161,23 +163,41 @@
             // 
             // ticketPath_PB
             // 
-            this.ticketPath_PB.Location = new System.Drawing.Point(258, 34);
+            this.ticketPath_PB.Location = new System.Drawing.Point(2, 275);
             this.ticketPath_PB.Name = "ticketPath_PB";
             this.ticketPath_PB.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.ticketPath_PB.Size = new System.Drawing.Size(235, 23);
             this.ticketPath_PB.TabIndex = 19;
-            this.ticketPath_PB.Text = "Emplacement sauvegarde ticket";
+            this.ticketPath_PB.Text = "Choisir dossier sauvegarde ticket";
             this.ticketPath_PB.UseVisualStyleBackColor = true;
             this.ticketPath_PB.Click += new System.EventHandler(this.ticketPath_PB_Click);
             // 
-            // ticketPathRecover_FD
+            // button1
             // 
+            this.button1.Location = new System.Drawing.Point(2, 238);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(231, 31);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Choisir base de donnée produits";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(565, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 17);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "kg";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 310);
+            this.ClientSize = new System.Drawing.Size(702, 315);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.ticketPath_PB);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -214,6 +234,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button ticketPath_PB;
         private System.Windows.Forms.FolderBrowserDialog ticketPathRecover_FD;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
     }
 }
 
